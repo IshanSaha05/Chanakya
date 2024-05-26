9fac45dca8195e1fc1aff3786216a50595a73a41
Ishan Saha <63413315+IshanSaha05@users.noreply.github.com>
2024-05-27 01:50:01 +0530

    Delete Election-Chanakyya directory

--- Election-Chanakyya/pkg/config/config.go
+++
@@ -1,7 +0,0 @@
-package config
-
-var ScrapeSiteUrlAssembly = "http://www.chanakyya.com/Election-Results?electionType=Assembly"
-var ScrapeSiteUrlParliament = "http://www.chanakyya.com/Election-Results?electionType=Parliament"
-var ScrapeSiteUrlDistrict = "http://www.chanakyya.com/Election-Results?electionType=District"
-
-var MongoSite = "mongodb://localhost:27017"
